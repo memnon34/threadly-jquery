f9dbf1a1a0f9d579610c80c83b9a0b6cb2548068
Daniel Llinas <danllinas@gmail.com>
2015-07-09 17:48:12 -0400

    Added initial files for Wyncode exercise.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var main = function() {
   $('form').submit(function() {
-    
+    var comment = $('#comment').val(); 
+
     if(comment !== "") {
       var html = $('<li>').text(comment);
-      
+      html.prependTo('.comments');
+      $('#comment').val("");
     }
 
     return false;
